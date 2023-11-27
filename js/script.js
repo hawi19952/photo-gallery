8115fb6acc314b13e3c2162cd4af3158ff15c01f
hawi19952 <hussain.ali@hey.com>
2023-11-27 19:30:28 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function changeBackground(elm) {
+  const imageSrc = elm.src;
+  const altText = elm.alt;
+  let element = document.getElementById("message");
+  element.style.backgroundImage = `url(${imageSrc})`
+  element.innerHTML = altText;
+}
+
+function resetToDefault() {
+  const defaultMessage = "Hover over an image to display the alt text.";
+  let element = document.getElementById("message");
+  element.style.backgroundImage = "none"
+  element.innerHTML = defaultMessage;
+}
+
+console.log('hi from script')
